e37d51a0f831cc8ae4f574c27e1fe4461059596a
tkex <tkex@users.noreply.github.com>
2023-11-17 12:14:47 +0100

    feat: add basic colour constants

--- astar-inve.py
+++ astar-inve.py
@@ -3,3 +3,11 @@ import pygame
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Visualisierung - Beispiel")
+
+# Colour Constants in RGB
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+BLUE = (0, 0, 255)
